f23ab8450a383787e2bbde21c8e04765b8f54046
natsukaze4545 <40604302+natsukaze4545@users.noreply.github.com>
2020-04-12 04:32:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def handle_message(event):
         line_bot_api.reply_message(msg_from,TextSendMessage(text='まずは貯めるのか使うのかを教えてね！\n貯める or 使う'))
         setting2['setting1'] = True
         setting_['ID'] = user_id
-        line_bot_api.reply_message(msg_from,TextSendMessage(text=setting_['ID']))
+        print(setting_['ID'])
 
 
     if msg_text == '貯める' and setting2['setting1'] == True:
