636ee30024e3f7671dc5639293bfed6ca626d933
natsukaze4545 <40604302+natsukaze4545@users.noreply.github.com>
2020-04-14 02:01:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import psycopg2
 import random
 
+from datetime import datetime as dt
 
 def get_connection():
     dsn = os.environ.get('DATABASE_URL')
@@ -96,7 +97,14 @@ setting2 = {
 	'setting3':False,
 	'setting4':False,
 	'setting5':False,
+	'setting6':False,
+	'setting7':False,
+	'setting8':False,
+	'setting9':False,
+	'setting10':False,
 }
+
+
 
 Time = {
     'count':0,
@@ -122,8 +130,14 @@ date = {
 }
 '''
 date = {}
+
+pdate = {
+    'save': True,
+    'date': '',
+    'point':0
+    }
 def namecheck(ID,name):
-    random_id = random.randint(1,9999)
+    random_id = random.randint(1,999999)
     point = None
     conn = get_connection()
     cur = conn.cursor()
@@ -195,6 +209,71 @@ def seve(ID):
     with open('date.json','w') as f:
         json.dump(date, f)
     '''
+
+def seve2(ID,point):
+    try:
+        print('ok2')
+        print(setting_[ID]['dbID'])
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("ROLLBACK")
+        conn.commit()
+        cur.execute('SELECT * FROM botdb')
+        point_ = namecheck(ID,pdate[ID]['name'])
+        point2 = point_ + point
+        str_point = str(point)
+        for row in cur:
+            if ID+setting_[ID]['name'] in row:
+                dbID = row[0]
+                print('ok3')
+                cur.execute("UPDATE botdb SET point = '{point}' WHERE id='{dbID}';".format(point=str_point,dbID=dbID))
+                conn.commit()
+                print('ok3-2')
+                return
+        cur.execute("UPDATE botdb SET point = '{point}' WHERE id='{dbID}';".format(point=str_point,dbID=setting_[ID]['dbID']))
+        conn.commit()
+        print('ok4')
+    except Exception as e:
+        print (str(e))
+
+def seve3(ID):
+    try:
+        tdatetime = dt.now()
+        tstr = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
+        random_id = random.randint(1,999999)
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("ROLLBACK")
+        conn.commit()
+        if pdate[ID]['save'] == True:
+            date = '追加:'+pdate[ID]['date']
+        if pdate[ID]['save'] == False:
+            date = '削除:'+pdate[ID]['date']
+        cur.execute("insert into botdb values({id},'{user_id}','{date}','{datetime}')".format(id=random_id,user_id=ID+name+'point',date=date,datetime=tstr))
+        #cur.execute("insert into botdb values({id},'{user_id}','{date}','{datetime}')".format(id=random_id,user_id=ID+name+'point',date='追加:早起き:10ポイント',datetime='2020-4-14 00:00:00'))
+        conn.commit()
+    except Exception as e:
+        print (str(e))
+
+def pointcheck(ID,name):
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("ROLLBACK")
+        conn.commit()
+        cur.execute('SELECT * FROM botdb')
+        date_list = []
+        for row in cur:
+            if ID+name+'point' in row:
+                date = row[2]
+                tstr = row[3]
+                tdatetime = dt.strptime(tstr, '%Y-%m-%d %H:%M:%S')
+                list_ = [date,tdatetime]
+                date_list.append(list_)
+        d = sorted(date_list, key=lambda s: s[1], reverse=True)
+        return d
+    except Exception as e:
+        print (str(e))
 
 
 #環境変数取得
@@ -363,34 +442,35 @@ def handle_message(event):
     user_id = event.source.user_id
     if msg_text == '設定する':
         line_bot_api.reply_message(msg_from,TextSendMessage(text='まずは貯めるのか使うのかを教えてね！\n貯める or 使う'))
-        setting2['setting1'] = True
         setting_[user_id] = {'use':True,'name':'name','point':0,'time':0,'timepoint':0,'ID':'','point2':0,'dbID':0}
         setting_[user_id]['ID'] = user_id
         Time[user_id] = {'count':0,'pointcount_1':0,'pointcount_2':0,'pointcount2_1':0,'pointcount2_2':0}
+        setting2[user_id] = {'setting1':False,'setting2':False,'setting3':False,'setting4':False,'setting5':False,'setting6':False,'setting7':False,'setting8':False,'setting9':False,'setting10':False,}
         set_ = 2
 
         stoptime = 0
 
         stoppoint = 0
+        setting2[user_id]['setting1'] = True
 
 
-    if msg_text == '貯める' and setting2['setting1'] == True and user_id == setting_[user_id]['ID']:
+    if msg_text == '貯める' and setting2[user_id]['setting1'] == True and user_id == setting_[user_id]['ID']:
         setting_[user_id]['use'] = False
-        setting2['setting1'] = False
-        setting2['setting2'] = True
+        setting2[user_id]['setting1'] = False
+        setting2[user_id]['setting2'] = True
         line_bot_api.reply_message(msg_from,TextSendMessage(text='OK！貯めるに設定したよ！\n次は行う人の名前を教えてね！(ニックネーム可)\n[打ち方] 名前:"行う人の名前"\n例: たろうくんの場合　名前:たろう'))
 
 
-    if msg_text == '使う' and setting2['setting1'] == True and user_id == setting_[user_id]['ID']:
+    if msg_text == '使う' and setting2[user_id]['setting1'] == True and user_id == setting_[user_id]['ID']:
         setting_[user_id]['use'] = True
-        setting2['setting1'] = False
-        setting2['setting2'] = True
+        setting2[user_id]['setting1'] = False
+        setting2[user_id]['setting2'] = True
         line_bot_api.reply_message(msg_from,TextSendMessage(text='OK！使うに設定したよ！\n次は行う人の名前を教えてね！(ニックネーム可)\n[打ち方] 名前:"行う人の名前"\n例: たろうくんの場合　名前:たろう'))
 
 
-    if '名前' in msg_text and setting2['setting2'] == True and user_id == setting_[user_id]['ID']:
-        setting2['setting2'] = False
-        setting2['setting3'] = True
+    if '名前' in msg_text and setting2[user_id]['setting2'] == True and user_id == setting_[user_id]['ID']:
+        setting2[user_id]['setting2'] = False
+        setting2[user_id]['setting3'] = True
         name = msg_text.replace("名前:","")
         setting_[user_id]['name'] = name
         point = namecheck(user_id,name)
@@ -401,17 +481,17 @@ def handle_message(event):
             line_bot_api.reply_message(msg_from,TextSendMessage(text='OK！今までの合計ポイントは{}だよ！\n次は1分間に消費するポイント数を設定してね！\n[打ち方] ポイント:"一分当たりの消費ポイント(数字)"\n例: 3ポイントの場合　ポイント:3'.format(point)))
 
 
-    if 'ポイント' in msg_text and setting2['setting3'] == True and user_id == setting_[user_id]['ID']:
-        setting2['setting3'] = False
-        setting2['setting4'] = True
+    if 'ポイント' in msg_text and setting2[user_id]['setting3'] == True and user_id == setting_[user_id]['ID']:
+        setting2[user_id]['setting3'] = False
+        setting2[user_id]['setting4'] = True
         str_timepoint = msg_text.replace("ポイント:","")
         timepoint = int(str_timepoint)
         setting_[user_id]['timepoint'] = timepoint
         line_bot_api.reply_message(msg_from,TextSendMessage(text='OK！{}ポイントに設定できたよ！\n最後に、何分行うか設定してね！\n[打ち方] 時間:"行う分数(数字)"\n例: 5分の場合　時間:5'.format(timepoint)))
 
 
-    if '時間' in msg_text and setting2['setting4'] == True and user_id == setting_[user_id]['ID']:
-        setting2['setting4'] = False
+    if '時間' in msg_text and setting2[user_id]['setting4'] == True and user_id == setting_[user_id]['ID']:
+        setting2[user_id]['setting4'] = False
         str_time = msg_text.replace("時間:","")
         int_time = int(str_time)
         setting_[user_id]['time'] = int_time
@@ -474,6 +554,83 @@ def handle_message(event):
         if setting_[user_id]['use'] == True:
             line_bot_api.reply_message(msg_from,TextSendMessage(text='残り時間 : {count}\n経過ポイント : {pointcount_1}\n合計ポイント : {pointcount_2}'.format(count=Time[user_id]['count'],pointcount_1=Time[user_id]['pointcount2_1'],pointcount_2=Time[user_id]['pointcount2_2'])))
             setting_[user_id]['point2'] = Time[user_id]['pointcount2_1']
+
+
+    if 'ポイント追加' == msg_text:
+        setting2[user_id] = {'setting1':False,'setting2':False,'setting3':False,'setting4':False,'setting5':False,'setting6':False,'setting7':False,'setting8':False,'setting9':False,'setting10':False,}
+        line_bot_api.reply_message(msg_from,TextSendMessage(text='まずはやったひとの名前を教えてね！\n[打ち方]　やったひと:"名前"\n例: たろうくんの場合  やったひと:たろう'))
+        setting2[user_id]['setting9'] = True
+        pdate[user_id] = {'save': True,'date': '','point':'','name':''}
+
+
+    if 'ポイント削除' == msg_text:
+        setting2[user_id] = {'setting1':False,'setting2':False,'setting3':False,'setting4':False,'setting5':False,'setting6':False,'setting7':False,'setting8':False,'setting9':False,'setting10':False,}
+        line_bot_api.reply_message(msg_from,TextSendMessage(text='まずはやったひとの名前を教えてね！\n[打ち方]　やったひと:"名前"\n例: たろうくんの場合  やったひと:たろう'))
+        setting2[user_id]['setting10'] = True
+        pdate[user_id] = {'save': False,'date': '','point':'','name':''}
+
+    if 'やったひと' in msg_text:
+        date = msg_text.replace("やったひと:","")
+        if setting2[user_id]['setting9'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}さんがしたんだね！\n\n次はやったことを教えてね！\n[打ち方]　やったこと:"やったこと"\n例:  早起きムした場合  やったこと:早起き'.format(date=date)))
+            pdate[user_id]['name'] = date
+            setting2[user_id]['setting9'] = False
+            setting2[user_id]['setting5'] = True
+        if setting2[user_id]['setting10'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}さんがしたんだね！\n\n次はやったことを教えてね！\n[打ち方]　やったこと:"やったこと"\n例:  ゲームした場合  やったこと:ゲーム'.format(date=date)))
+            pdate[user_id]['name'] = date
+            setting2[user_id]['setting10'] = False
+            setting2[user_id]['setting6'] = True
+
+
+    if 'やったこと' in msg_text:
+        date = msg_text.replace("やったこと:","")
+        if setting2[user_id]['setting5'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}をしたんだね！\n\n次は追加するポイントを教えてね！\n[打ち方]　ポイント:"ポイント数(数字)"\n例: 10ポイントの場合  ポイント:10'.format(date=date)))
+            pdate[user_id]['date'] = date
+            setting2[user_id]['setting5'] = False
+            setting2[user_id]['setting7'] = True
+        if setting2[user_id]['setting6'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}をしたんだね！\n\n次は削除するポイントを教えてね！\n[打ち方]　ポイント:"ポイント数(数字)"\n例: 10ポイントの場合  ポイント:10'.format(date=date)))
+            pdate[user_id]['date'] = date
+            setting2[user_id]['setting6'] = False
+            setting2[user_id]['setting8'] = True
+
+    if 'ポイント' in msg_text:
+        point = msg_text.replace("ポイント:","")
+        if setting2[user_id]['setting7'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}をしたから{point}追加されたよ！\n\n今までの記録は  記録確認  で見れるよ！'.format(date=pdate[user_id]['date'],point=point)))
+            pdate[user_id]['point'] = point
+            setting2[user_id]['setting7'] = False
+            seve2(user_id,int(point))
+            seve3(user_id)
+        if setting2[user_id]['setting8'] == True:
+            line_bot_api.reply_message(msg_from,TextSendMessage(text='{date}をしたから{point}削除したよ！\n\n今までの記録は  記録確認  で見れるよ！'.format(date=pdate[user_id]['date'],point=point)))
+            setting2[user_id]['setting8'] = False
+            int_point = int(point)
+            point2 = -int_point
+            pdate[user_id]['point'] = str(point2)
+            seve2(user_id,point2)
+            seve3(user_id)
+
+
+    if '記録確認' == msg_text:
+        line_bot_api.reply_message(msg_from,TextSendMessage(text='確認したい人の名前を教えてね！\n[打ち方]　確認:"名前"\n例: たろうくんの場合  確認:たろう'))
+
+
+    if '確認:' in msg_text:
+        name = msg_text.replace("確認:","")
+        d = pointcheck(user_id,name)
+        list2 = []
+        for t in d:
+            str_datetime = t[1].strftime('%Y-%m-%d %H:%M:%S')
+            list2.append([t[0],str_datetime])
+        list3 = []
+        for t in list2:
+            list3.append(' : '.join(t))
+        date_str = '\n\n'.join(list3)
+        line_bot_api.reply_message(msg_from,TextSendMessage(text='[今までの記録]\n{date_str}\n今の合計ポイント : {point}'.format(date_str=date_str,point=namecheck(user_id,name))))
+
 
 
 
